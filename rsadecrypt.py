8f5f4dc9af99bf50673db0044e1bddf485cd635c
Gabriel Pinheiro <pinheiro.gbr@gmail.com>
2019-05-17 22:43:31 -0300

    initialize

---
+++ rsadecrypt.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+n = 3386871005523622783
+e = 65537
+
+# n = p . q
+#p = ?
+#q = ?
+
+#qq = (p.1)(q.1) = z
+#d.e mod(qq) = 1
+
+w = 1
+
+for x in xrange(n):
+    x = x+2
+    if x == 0:
+        print(x)
+        break
+    k = n-x
+    if k == x:
+        break
+    else:
+        f = k % x
+        if f == 0:
+            print(x, k)
+    
+print("Banan")
